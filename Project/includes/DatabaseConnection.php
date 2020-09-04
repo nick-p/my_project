81607f60fb6f1d094edd2c124d69c44570effa49
Nick Plain <nick@nickplain.com>
2020-09-04 15:40:09 +0100

    Added DatabaseFunctions.php

--- Project/includes/DatabaseConnection.php
+++ Project/includes/DatabaseConnection.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:localhost;dbname=ijdb; charset=utf-8', 'ijdbuser', 'mypassword');
+$pdo = new PDO('mysql:host=localhost;dbname=ijdb; charset=utf8', 'ijdbuser', 'mypassword');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
