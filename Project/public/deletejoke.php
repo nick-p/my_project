b39a2ee16b43e08b2c71efdeef4ac39eb2820579
Nick Plain <nick@nickplain.com>
2020-09-09 10:52:58 +0100

    added deleteJoke function

--- Project/public/deletejoke.php
+++ Project/public/deletejoke.php
@@ -3,10 +3,7 @@
 try {
     include __DIR__ . '/../includes/DatabaseConnection.php';
 
-    $sql = 'DELETE FROM `joke` WHERE `id` = :id';
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':id', $_POST['id']);
-    $stmt->execute();
+    deleteJoke($pdo, $_POST['id']);
 
     header('location: jokes.php');
 
