3ea3d423bf6c8d933b427616a9ee8a9d35aa69a9
Nick Plain <nick@nickplain.com>
2020-09-09 10:59:09 +0100

    added allJokes function

--- Project/includes/DatabaseFunctions.php
+++ Project/includes/DatabaseFunctions.php
@@ -31,3 +31,17 @@ function updateJoke($pdo, $jokeId, $joketext, $authorId) {
     query($pdo, 'UPDATE `joke` SET `authorId` = :authorId, `joketext` = :joketext
         WHERE `id` = :id', $parameters);
 }
+
+function deleteJoke($pdo, $id) {
+    $parameters = [':id' => $id];
+
+    query($pdo, 'DELETE FROM `joke` WHERE `id` = :id', $parameters);
+}
+
+function allJokes($pdo) {
+    $jokes = query($pdo, 'SELECT `joke`.`id`, `joketext`, `name`, `email`
+        FROM `joke` INNER JOIN `author`
+        ON `authorid` = `author`.`id`');
+    
+    return $jokes->fetchAll();
+}
